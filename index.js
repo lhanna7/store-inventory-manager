16844fab36f81c9281182098aad8e905fe674c68
Lauren <lhanna7@ford.com>
2022-04-25 20:09:50 -0700

    start js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const itemName = document.querySelector(`.item-name`)
-const itemsInInventory = document.querySelector(`.items-in-inventory`)
+
+const form = document.querySelector(".form")
+const submit = document.querySelector("#submit")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const itemName = formData.get(`item-name`)
+    const itemSell = formData.get(`sell-in`)
+    const itemQuality = formData.get(`quality`)
+    const itemInventory = document.querySelector("items-in-inventory")
+
+
+
+})
