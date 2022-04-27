cd8988d80e7e4f6d2c226fde0ae8118f3668cdb2
Lauren <lhanna7@ford.com>
2022-04-27 11:20:25 -0700

    how tough are ya?

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const form = document.querySelector(".form")
 const submit = document.querySelector("#submit")
 const inventory = document.querySelector(".items-in-inventory")
 
-let items = ""
+let items = {}
 form.addEventListener("submit", (event) => {
     event.preventDefault()
     const formData = new FormData(event.target)
 
-    let items = {
+    items = {
         itemName: formData.get(`item-name`),
         itemSell: formData.get(`sell-in`),
         itemQuality: formData.get(`quality`),
@@ -22,4 +22,42 @@ form.addEventListener("submit", (event) => {
     <p>${items.itemSell}</p>
     <p>${items.itemQuality}</p>
     `
+})
+
+const imageContainer = document.querySelector(".img-container")
+const imageBox = document.createElement("div")
+let clicks = 0
+
+function onClickPlus() {
+    clicks += 1;
+    if (clicks == 1) {
+        imageContainer.append(imageBox)
+        imageBox.innerHTML = `
+        <img src="images/nextday.jpg" alt="day1">
+      `
+    } else if (clicks == 2) {
+        imageBox.innerHTML = `
+        <img src="images/twodays.jpg" alt="day1">
+      `
+    } else if (clicks == 3) {
+        imageBox.innerHTML = `
+        <img src="images/threedays.jpg" alt="day1">
+      `
+    } else {
+        imageBox.innerHTML = `
+        <img src="images/somuchlater.jpg" alt="day1">
+      `
+    }
+}
+
+const nextDay = document.querySelector(".next")
+nextDay.addEventListener("click", () => {
+    nextDay.style.color = "blue";
+    onClickPlus()
+})
+
+const prevDay = document.querySelector(".prev")
+nextDay.addEventListener("click", () => {
+    nextDay.style.color = "blue";
+    onClickMinus()
 })
